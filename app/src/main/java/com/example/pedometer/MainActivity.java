041d8255a9131bb69ddec0ea2b3ca1abaef003c0
Indranil Mandal <37973310+imandal665@users.noreply.github.com>
2021-12-24 00:35:42 +0530

    update
    update

--- app/src/main/java/com/example/pedometer/MainActivity.java
+++ app/src/main/java/com/example/pedometer/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.pedometer;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.util.Log;
@@ -26,13 +27,14 @@ public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
     //    @BindView(R.id.steps_textView)
-    TextView stepsTextView;
+//    TextView stepsTextView;
     private long steps;
 
     @BindView(R.id.walking_animation)
     LottieAnimationView lottieAnimationView;
-    @BindView(R.id.main_activity_toolbar)
-    Toolbar toolbar;
+    //    @BindView(R.id.main_activity_toolbar)
+//    Toolbar toolbar;
+    MediaPlayer mediaPlayer;
 
     private PedoSenserManager pedoSenserManager;
     private boolean endTimerActive = false;
@@ -43,9 +45,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 //        setTheme(R.style.MainActivity);
         ButterKnife.bind(this);
-        setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(this.getResources().getColor(R.color.colorPrimaryDark));
-        stepsTextView = findViewById(R.id.steps_textView);
+//        setSupportActionBar(toolbar);
+        mediaPlayer = MediaPlayer.create(this, R.raw.stay);
+//        toolbar.setTitleTextColor(this.getResources().getColor(R.color.colorPrimaryDark));
+//        stepsTextView = findViewById(R.id.steps_textView);
         EventBus.getDefault().register(this);
         pedoSenserManager = new PedoSenserManager(MainActivity.this);
         updateSteps(new UpdateStepChangesInDisplay());
@@ -53,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void updateSteps(UpdateStepChangesInDisplay updateStepChangesInDisplay) {
-        stepsTextView.setText("Steps Taken - " + String.valueOf(pedoSenserManager.getSteps()));
+//        stepsTextView.setText("Steps Taken - " + String.valueOf(pedoSenserManager.getSteps()));
         if (pedoSenserManager.getSteps() != 0) {
             Log.i(TAG, String.valueOf(lottieAnimationView.getSpeed()));
             if (!endTimerActive)
@@ -61,47 +64,67 @@ public class MainActivity extends AppCompatActivity {
             endAnimationTimer();
         }
 
+
     }
 
     private void beginAnimationTimer() {
-        new CountDownTimer(10000, 1000) {
-
-
+        new CountDownTimer(10000, 100) {
             @Override
             public void onTick(long millisUntilFinished) {
 
                 if (millisUntilFinished > 0) {
-
                     lottieAnimationView.setSpeed(1.0f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(100f));
                 }
                 if (millisUntilFinished > 1000) {
                     lottieAnimationView.setSpeed(0.9f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(90f));
                 }
                 if (millisUntilFinished > 2000) {
                     lottieAnimationView.setSpeed(0.8f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(80f));
                 }
                 if (millisUntilFinished > 3000) {
                     lottieAnimationView.setSpeed(0.7f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(70f));
                 }
                 if (millisUntilFinished > 4000) {
                     lottieAnimationView.setSpeed(0.6f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(60f));
                 }
                 if (millisUntilFinished > 5000) {
                     lottieAnimationView.setSpeed(0.5f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(50f));
                 }
                 if (millisUntilFinished > 6000) {
                     lottieAnimationView.setSpeed(0.4f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(40f));
                 }
                 if (millisUntilFinished > 7000) {
                     lottieAnimationView.setSpeed(0.3f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(30f));
                 }
                 if (millisUntilFinished > 8000) {
                     lottieAnimationView.setSpeed(0.2f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(20f));
                 }
                 if (millisUntilFinished > 9000) {
                     if (!lottieAnimationView.isAnimating())
                         lottieAnimationView.playAnimation();
                     lottieAnimationView.setSpeed(0.1f);
+
+                    Thread thread = new Thread() {
+                        @Override
+                        public void run() {
+                            if (!mediaPlayer.isPlaying()) {
+//                        mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(10f));
+                                mediaPlayer.start();
+                            }
+                        }
+                    };
+                    thread.start();
+
+
                 }
             }
 
@@ -114,32 +137,40 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void endAnimationTimer() {
-        new CountDownTimer(35000, 1000) {
+        new CountDownTimer(10000, 100) {
             public void onTick(long millisUntilFinished) {
                 endTimerActive = true;
                 if (millisUntilFinished < 8000) {
                     lottieAnimationView.setSpeed(0.8f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(80f));
                 }
                 if (millisUntilFinished < 7000) {
                     lottieAnimationView.setSpeed(0.7f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(70f));
                 }
                 if (millisUntilFinished < 6000) {
                     lottieAnimationView.setSpeed(0.6f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(60f));
                 }
                 if (millisUntilFinished < 5000) {
                     lottieAnimationView.setSpeed(0.5f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(50f));
                 }
                 if (millisUntilFinished < 4000) {
                     lottieAnimationView.setSpeed(0.4f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(40f));
                 }
                 if (millisUntilFinished < 3000) {
                     lottieAnimationView.setSpeed(0.3f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(30f));
                 }
                 if (millisUntilFinished < 2000) {
                     lottieAnimationView.setSpeed(0.2f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(20f));
                 }
                 if (millisUntilFinished < 1000) {
                     lottieAnimationView.setSpeed(0.1f);
+//                    mediaPlayer.setPlaybackParams(mediaPlayer.getPlaybackParams().setSpeed(10f));
                 }
 
             }
@@ -147,6 +178,16 @@ public class MainActivity extends AppCompatActivity {
             public void onFinish() {
                 endTimerActive = false;
                 lottieAnimationView.setSpeed(0f);
+                Thread thread = new Thread() {
+                    @Override
+                    public void run() {
+                        if (!mediaPlayer.isPlaying()) {
+                            if (mediaPlayer.isPlaying())
+                                mediaPlayer.pause();
+                        }
+                    }
+                };
+                thread.start();
 
             }
         }.start();
